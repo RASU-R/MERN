41a6290f397af6de907fb2c3dfd3a713b354311c
Rasu R <108165654+RASU-R@users.noreply.github.com>
2023-06-11 16:05:53 +0530

    Add files via upload

---
+++ Sociopedia/client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,36 @@
+import { Typography,useTheme} from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper"; 
+const AdvertWidget=()=>{
+const {palette}=useTheme();
+const dark=palette.neutral.dark;
+const main=palette.neutral.main;
+const medium=palette.neutral.medium;
+
+
+return (
+    <WidgetWrapper>
+        <FlexBetween>
+            <Typography color={dark} variant="h5" fontWeight="500">
+                Sponsored
+            </Typography>
+            <Typography color={medium}>Create Ad</Typography>
+        </FlexBetween>
+        <img 
+          width="100%"
+          height="auto"
+          alt="advert"
+          src="http://localhost:3001/assets/info.jpg"
+          style={{ borderRadius:"0.75rem", margin:"0.74rem 0"}}
+        />
+        <FlexBetween>
+            <Typography color={main}>Cricket Knowledge</Typography>
+            <Typography color={medium}>cricacademy.com</Typography>
+        </FlexBetween>
+        <Typography color={medium} m="0.5rem 0">
+            Setting up for big bright future in cricket...To enlightening your cricket career in correct way..!
+        </Typography>
+    </WidgetWrapper>
+)
+};
+export default AdvertWidget;
